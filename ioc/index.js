0e65c13ea26b8247579a15c5ec4efa14db7a447f
huxf <huxf@dxy.cn>
2017-10-30 17:47:12 +0800

    a

--- ioc/index.js
+++ ioc/index.js
@@ -1,1 +1,18 @@
+class Ioc {
+    constructor() {
+        this.container = [];
+    }
 
+    bind(name, load) {
+        this.container[name] = load;
+    }
+
+}
+
+var ioc = new Ioc();
+
+ioc.bind('qq', function() {
+    console.log(123);
+});
+
+console.log(ioc);

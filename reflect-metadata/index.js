57ac1289a45a1179177a4a8b6f5b8e93f337a369
黑眉 <heimei@qunhemail.com>
2018-05-25 10:41:23 +0800

    a

---
+++ reflect-metadata/index.js
@@ -0,0 +1,31 @@
+require('reflect-metadata');
+
+class Example {
+// property declarations are not part of ES6, though they are valid in TypeScript:
+// static staticProperty;
+// property;
+
+    constructor(p) { }
+    static staticMethod(p) { }
+    method(p) { }
+}
+
+const options = 1;
+
+// constructor
+Reflect.defineMetadata("custom:annotation", options, Example);
+// property (on constructor)
+Reflect.defineMetadata("custom:annotation", options, Example, "staticProperty");
+
+// property (on prototype)
+Reflect.defineMetadata("custom:annotation", options, Example.prototype, "property");
+
+// method (on constructor)
+Reflect.defineMetadata("custom:annotation", options, Example, "staticMethod");
+
+// method (on prototype)
+Reflect.defineMetadata("custom:annotation", options, Example.prototype, "method");
+
+let result = Reflect.getMetadata('custom:annotation', Example, "staticMethod");
+
+console.log(Example, result);

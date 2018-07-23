cacb78de3b0bd411dc022a36b45d7ffb1a593a58
黑眉 <heimei@qunhemail.com>
2018-07-23 18:50:42 +0800

    a

--- javascript/shared-worker/index.js
+++ javascript/shared-worker/index.js
@@ -3,7 +3,12 @@ console.log(worker);
 
 worker.port.postMessage('get');
 worker.port.postMessage('123123');
+worker.port.postMessage('get');
+worker.port.postMessage('123');
+worker.port.postMessage('get');
 
 worker.port.onmessage = (e) => {
     console.log(e);
 }
+
+worker.port.messageerror = (e) => console.log;
--- javascript/shared-worker/worker.js
+++ javascript/shared-worker/worker.js
@@ -1,11 +1,16 @@
 let workerResult = '';
 
+let worker = [];
+
 onconnect = function (e) {
     const port = e.ports[0];
 
     port.onmessage = (e) => {
         if (e.data !== 'get') {
-            workerResult = 'Result: ' + e.data.toString();
+            worker.push(e.data.toString());
+            workerResult = 'Add: success';
+        } else {
+            workerResult = JSON.stringify(worker);
         }
         port.postMessage(workerResult);
     }

c3f3f1403907c05d583bbec5eee16a47f374f72e
黑眉 <heimei@qunhemail.com>
2018-09-30 18:34:32 +0800

    a

---
+++ clojure/README.md
@@ -0,0 +1,14 @@
+### vscode Clojure-lint
+
+1. map {} (hash-map) (get) (get-in)
+2. vectors 类似 array [] (get) (vector) (conj)
+3. list '() (nth list index) (list ) (conj list or item)
+4. set #{} (hash-set ) (conj ) (set ) (contains? ) (get )
+5. reg #""
+6. loop recur
+7. defn def
+8. reduce
+
+(:gen-class) 允许程序命令行执行
+declare 变量提升？
+defmacro macroexpand 宏定义
--- clojure/my-clojure/src/my_clojure/core.clj
+++ clojure/my-clojure/src/my_clojure/core.clj
@@ -1,13 +1,90 @@
-(ns my-clojure.core
-  (:gen-class))
+(ns my-clojure.core)
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def filename "suspects.csv")
+(def vamp-keys [:name :glitter-index])
+(defn str->int
+  [str]
+  (Integer. str))
 
-(if true
-  (do (println "Success!")
-      "By Zeus's hammer!")
-  (do (println "Failure!")
-      "By Aquaman's trident!"))
+(def conversions {:name identity
+                  :glitter-index str->int})
+
+(defn convert
+  [vamp-key value]
+  ((get conversions vamp-key) value))
+
+(convert :glitter-index "3")
+
+(defn parse
+  [string]
+  (map #(clojure.string/split % #",")
+       (clojure.string/split string #"\n")))
+
+(defn mapify
+  [rows]
+  (map (fn [unmapped-row]
+         (reduce (fn [row-map [vamp-key value]]
+                   (assoc row-map vamp-key (convert vamp-key value)))
+                 {}
+                 (map vector vamp-keys unmapped-row)))
+       rows))
+
+(first (mapify (parse (slurp filename))))
+
+(defn glitter-filter
+  [minimum-glitter records]
+  (filter #(>= (:glitter-index %) minimum-glitter) records))
+
+(glitter-filter 3 (mapify (parse (slurp filename))))
+
+; demo1
+(defn dec-maker
+  "entry number to create dec function"
+  [dec-number]
+  #(- %1 dec-number))
+
+(def dec9 (dec-maker 9))
+(dec9 10)
+
+; demo2
+(defn mapset
+  "entry fn and vector return set"
+  [fn arr]
+  (loop [remaing arr
+         result []]
+    (if (empty? remaing)
+      (set result)
+      (let [[first & remaing] remaing]
+        (recur remaing
+               (conj result (fn first)))))))
+
+(mapset inc [1 1 2 2])
+
+; demo2-1
+(defn mapset
+  "entry fn and vector return set"
+  [fn arr]
+  (set (map fn arr)))
+
+(mapset inc [1 1 2 2])
+
+(ns the-divine-cheese-code.visualization.svg
+  (:require [clojure.string :as s])
+  (:refer-clojure :exclude [min max]))
+
+(defn comparator-over-maps
+  [comparison-fn ks]
+  (fn [maps]
+    (zipmap ks
+            (map (fn [k] (apply comparison-fn (map k maps)))
+                 ks))))
+
+(def min (comparator-over-maps clojure.core/min [:a :b]))
+(def max (comparator-over-maps clojure.core/max [:a :b]))
+(min [{:a 1 :b 3} {:a 5 :b 0}])
+
+(defmacro backwards
+  [form]
+  (reverse form))
+
+(backwards (" backwards" " am" "I" str))
